b281b1e25fdb184d573f52320d6180ac6736e677
Jakob Marorne <jakob@jakobmarrone.com>
2025-05-06 16:33:11 -0700

    Initial Commit

---
+++ Alldun/AlldunApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlldunApp.swift
+//  Alldun
+//
+//  Created by Jakob Marrone on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AlldunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
